f89f838bee9ea7ffb80b45833787db02a3ee7b8d
skver <nulla.animo@gmail.com>
2015-10-09 11:21:15 +0300

    schema.org

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -13,69 +13,77 @@
 				<li class="name_bread">  {{ $tire->size}} {{ $tire->brand}} {{ $tire->model}} </li>
 			</ul>
 		</div>
-		<div class="cart-cart">
+		<div class="cart-cart itemscope itemtype="http://schema.org/Product"">
 
-					@if ($tire->class === 'Сельхоз шины') <div class="header_cart header_orange"> @endif
-					@if ($tire->class === 'Индустриальные шины') <div class="header_cart header_green"> @endif
-					@if ($tire->class === 'Грузовые шины') <div class="header_cart header_blue"> @endif
-					
-				<span class="cart_title_size">{{ $tire->size}}</span> <br>
-				<span class="cart_title_model">{{ $tire->model}} @if ($tire->class === 'Грузовые шины') <span class="truck_type">{{ $tire->type_arch}}</span> @endif</span>
-			</div>
-			<div class="row">
-				<div class="col-xs-6">
-					<div class="img_cart_cart">
-						<img src="{{ asset($tire->img_big) }}" alt="{{ $tire->size}} {{ $tire->brand}} {{ $tire->model}}">
-					</div>
+			<meta itemprop="brand" content="{{ $tire->brand }}">
+			<meta itemprop="category" content="{{ $tire->class }}">
+			<meta itemprop="name" content="{{ $tire->size }} {{ $tire->brand }} {{ $tire->model }}">
+
+			@if ($tire->class === 'Сельхоз шины') <div class="header_cart header_orange"> @endif
+			@if ($tire->class === 'Индустриальные шины') <div class="header_cart header_green"> @endif
+			@if ($tire->class === 'Грузовые шины') <div class="header_cart header_blue"> @endif
+
+			<span class="cart_title_size">{{ $tire->size}}</span> <br>
+			<span class="cart_title_model">{{ $tire->model}} @if ($tire->class === 'Грузовые шины') <span class="truck_type">{{ $tire->type_arch}}</span> @endif</span>
+		</div>
+		<div class="row">
+			<div class="col-xs-6">
+				<div class="img_cart_cart">
+					<img itemprop="image" src="{{ asset($tire->img_big) }}" alt="{{ $tire->size}} {{ $tire->brand}} {{ $tire->model}}">
+				</div>
+				<div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
 					<div class="price_cart">
 						{{ number_format($tire->price, 0, ',', ' ')  }} руб.
 					</div>
-					<div class="more_cart">
-						Наличие уточняйте по телефону
-					</div>
+					<meta itemprop="price" content="{{ $tire->price }}">
+					<meta itemprop="priceCurrency" content="RUB">
 				</div>
-				<div class="col-xs-6">
-					<table class="tech">
-						<tr>
-							<td class="bold">Назначение:</td>
-							<td>{{ $tire->class }}</td>
-						</tr>
-						<tr>
-							<td class="bold">Тип:</td>
-							<td>{{ $tire->type_arch }}</td>
-						</tr>
-						<tr>
-							<td class="bold">ИН/CC, слои:</td>
-							<td>{{ $tire->tech }}</td>
-						</tr>
-						<tr>
-							<td class="bold">Страна:</td>
-							<td>{{ $tire->country }}</td>
-						</tr>
-						<tr>
-							<td class="bold">К/БК:</td>
-							<td>{{ $tire->tube }}</td>
-						</tr>
-					</table>
-					<button href="#PRICE" class="callback_button" id="MAIN_BUTTON">Узнать цену с доставкой</button>
+				<div class="more_cart">
+					Наличие уточняйте по телефону
 				</div>
+			</div>
+			<div class="col-xs-6">
+				<table class="tech">
+					<tr>
+						<td class="bold">Назначение:</td>
+						<td>{{ $tire->class }}</td>
+					</tr>
+					<tr>
+						<td class="bold">Тип:</td>
+						<td>{{ $tire->type_arch }}</td>
+					</tr>
+					<tr>
+						<td class="bold">ИН/CC, слои:</td>
+						<td>{{ $tire->tech }}</td>
+					</tr>
+					<tr>
+						<td class="bold">Страна:</td>
+						<td>{{ $tire->country }}</td>
+					</tr>
+					<tr>
+						<td class="bold">К/БК:</td>
+						<td>{{ $tire->tube }}</td>
+					</tr>
+				</table>
+				<button href="#PRICE" class="callback_button" id="MAIN_BUTTON">Узнать цену с доставкой</button>
 			</div>
 		</div>
 	</div>
-	
-	<div class="mfp-hide">
-		<div class="price_form" id="PRICE">
-			<div class="header_price_form">
-				Куда доставить?	
-			</div>
-			
-			<form id="CALL_FORM">
+</div>
+
+<div class="mfp-hide">
+	<div class="price_form" id="PRICE">
+		<div class="header_price_form">
+			Куда доставить?	
+		</div>
+
+		<form id="CALL_FORM">
 			<div class="text_callback_form">
 				<input type="hidden" name="tire" value="{{ $tire->size }} {{ $tire->brand }} {{ $tire->model }}">
 				Мы посчитаем доставку транспортной кампанией в город 
 
 				<input type="text" requried name="city" required placeholder="Введите ваш город"> <br> и Вам перезвоним на номер: <input type="text" requried name="phone" required placeholder="Введите ваш номер"> <br>
-				 (через 10-15 минут)
+				(через 10-15 минут)
 				<br>
 				<input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
 				<button align='center' type="submit"  class="callback_form_button">Отправить</button>
--- resources/views/global.blade.php
+++ resources/views/global.blade.php
@@ -3,34 +3,42 @@
 @section('content')
 
 <div class="container-fluid padding-top-76">
-@foreach($tires as $tire)
+	@foreach($tires as $tire)
 	<div class="col-xs-3">
-		<div class="cart">
+		<div class="cart" itemscope itemtype="http://schema.org/Product">
 
-					@if ($tire->class === 'Сельхоз шины') <div class="header_cart header_orange"> @endif
-					@if ($tire->class === 'Индустриальные шины') <div class="header_cart header_green"> @endif
-					@if ($tire->class === 'Грузовые шины') <div class="header_cart header_blue"> @endif
+			@if ($tire->class === 'Сельхоз шины') <div class="header_cart header_orange"> @endif
+			@if ($tire->class === 'Индустриальные шины') <div class="header_cart header_green"> @endif
+			@if ($tire->class === 'Грузовые шины') <div class="header_cart header_blue"> @endif
 
-					<span class="cart_title_size">{{ $tire->size}}</span> 
-					 <br>
-					<span class="cart_title_model">{{ $tire->model}} @if ($tire->class === 'Грузовые шины') <span class="truck_type">{{ $tire->type_arch}}</span> @endif</span>
-					</div>
-					<div class="img_cart">
-						<a href="/catalog/{{ $tire->slug }}"><img src="{{ asset($tire->img_big) }}" alt="{{ $tire->size}} {{ $tire->brand}} {{ $tire->model}}"></a>
-					</div>
-					<div class="price">
-						{{ number_format($tire->price, 0, ',', ' ')  }} руб.
-					</div>
-					<div class="more">
-						<a href="/catalog/{{ $tire->slug }}">Подробнее...</a>
-					</div>
-					<div class="icon">
-						@if ($tire->class === 'Сельхоз шины') <img src="{{ asset("img/agri_icon.png") }}" alt="Сельхоз шина"> @endif
-						@if ($tire->class === 'Индустриальные шины') <img src="{{ asset("img/ind_icon.png") }}" alt="Индустриальная шина"> @endif
-						@if ($tire->class === 'Грузовые шины') <img src="{{ asset("img/truck_icon.png") }}" alt="Грузовая шина"> @endif		
-					</div>
+			<span class="cart_title_size">{{ $tire->size}}</span> 
+			<br>
+			<span class="cart_title_model" itemprop="model">{{ $tire->model}} @if ($tire->class === 'Грузовые шины') <span class="truck_type">{{ $tire->type_arch}}</span> @endif</span>
 		</div>
+		<div class="img_cart">
+			<a href="/catalog/{{ $tire->slug }}"><img itemprop="image" src="{{ asset($tire->img_big) }}" alt="{{ $tire->size}} {{ $tire->brand}} {{ $tire->model}}"></a>
+		</div>
+		<div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
+			<div class="price">
+				{{ number_format($tire->price, 0, ',', ' ')  }} руб.
+			</div>
+			<meta itemprop="price" content="{{ $tire->price }}">
+			<meta itemprop="priceCurrency" content="RUB">
+
+		</div>
+		<div class="more">
+			<a itemprop="url" href="/catalog/{{ $tire->slug }}">Подробнее...</a>
+		</div>
+		<div class="icon">
+			@if ($tire->class === 'Сельхоз шины') <img src="{{ asset("img/agri_icon.png") }}" alt="Сельхоз шина"> @endif
+			@if ($tire->class === 'Индустриальные шины') <img src="{{ asset("img/ind_icon.png") }}" alt="Индустриальная шина"> @endif
+			@if ($tire->class === 'Грузовые шины') <img src="{{ asset("img/truck_icon.png") }}" alt="Грузовая шина"> @endif		
+		</div>
+		<meta itemprop="brand" content="{{ $tire->brand }}">
+		<meta itemprop="category" content="{{ $tire->class }}">
+		<meta itemprop="name" content="{{ $tire->size }} {{ $tire->brand }} {{ $tire->model }}">
 	</div>
+</div>
 @endforeach
 </div>
 
