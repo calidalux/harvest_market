59c1c63edbf7dcf3000f9e2d232b98b0b5198e50
skver <nulla.animo@gmail.com>
2015-10-20 13:10:27 +0300

    добавление треллика

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -57,7 +57,7 @@ class WelcomeController extends Controller {
 
 		})->get();
 
-		$tires = $tires->sortByDesc('updated_at');
+		$tires = $tires->sortBy('sort_id'); //Сортировка
 
 		return view('global', compact('tires'));
 	}
