224d862eb6cf45ffaec535af10e4964e0f794266
calidalux <nulla.animo@gmail.com>
2015-10-21 22:20:54 +0300

    работа с pecom

--- app/Http/Controllers/GuzzleController.php
+++ app/Http/Controllers/GuzzleController.php
@@ -14,14 +14,48 @@ class GuzzleController extends Controller
     public function findTown()
     {
         $data = new PecomKabinet('harvestltd', '5A473F678485B056318A0E4B9E1E2FD42ECCC3D0');
-        $result = $data->call('branches', 'findbytitle', array('title' => 'Абакан'));
-        return $result;
+        $towns = [
+            'Тамбов',
+            'Нижний Новгород',
+            'Оренбург',
+            'Пенза',
+            'Уфа',
+            'Саранск',
+            'Казань',
+            'Самара',
+            'Саратов',
+            'Ульяновск',
+            'Чебоксары',
+            'Волгоград',
+            'Барнаул',
+            'Новосибирск',
+            'Омск',
+            'Горно-Алтайск',
+            'Абакан'
+        ];
+        foreach ($towns as $town) {
+            $result = $data->call('branches', 'findbytitle', array('title' => $town)); //Получаем информацию по названию города
+            $response = json_decode($result);
+            if (isset($response->items[0]->cityId)) {
+               $out[][$town] = $response->items[0]->cityId;
+            } else
+            {
+                $out[][$town] = $response->items[0]->branchId;
+            }
+            
+        }
+        var_dump($out);
+        return "Done!";
+        //return $response->items[0]->cityId;  
+
+         //$result = $data->call('branches', 'findbytitle', array('title' => 'Саратов')); //Получаем информацию по названию города
+         //return $result;
     }
 
     public function index()
     {          
         $main = new myPEC;
-        $data = $main->getCount(67083, 66729, "2015-10-22", 1, 1, 1, 1, 1, 1);
+        $data = $main->getCount(67083, 502, "2015-10-22", 1, 1, 1, 1, 1, 1);
 
         return $data;
         
